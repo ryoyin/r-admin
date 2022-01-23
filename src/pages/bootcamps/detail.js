70e71c310758763073c2bde6858dd4b93b36dbfe
Roy Ho <kwanyin2000@gmail.com>
2022-01-23 23:20:08 +0000

    bootcamp - remove and edit

--- src/pages/bootcamps/detail.js
+++ src/pages/bootcamps/detail.js
@@ -9,9 +9,13 @@ import {
     Breadcrumbs
 } from '@mui/material';
 
+import TextField from '@mui/material/TextField';
+import { CollectionsOutlined } from '@material-ui/icons';
+
 const Detail = () => {
 
     const [bootcamp, setBootcamp] = useState([])
+    const [editable, setEditable] = useState(false)
 
     const { id } = useParams()
 
@@ -29,9 +33,52 @@ const Detail = () => {
         })
     }
 
+    async function updateBootcamp() {
+        await axios.put(`http://localhost:9000/api/v1/bootcamps/${id}`, bootcamp)
+        .then(function (response) {
+            // handle success
+            window.location = `http://localhost:9000/api/v1/bootcamps/${id}`
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+    }
+
+    function editBootcamp() {
+        setEditable(true)
+    }
+
+    function cancelEditBootcamp() {
+        setEditable(false)
+    }
+
+    function saveBootcamp() {
+        console.log(bootcamp)
+        updateBootcamp()
+    }
+
+    function handleChange(event) {
+        console.log(event.target.id)
+        console.log(event.target.value)
+        bootcamp[event.target.id] = event.target.value
+        setBootcamp(bootcamp)
+    }
+
+    async function removeBootcamp() {
+        // {{URL}}/api/v1/bootcamps/5d725a1b7b292f5f8ceff788
+        await axios.delete(`http://localhost:9000/api/v1/bootcamps/${id}`)
+        .then(function() {
+            window.location = '/bootcamps'
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+    }
+
     useEffect(() => { 
         // make request for bootcamps
-
         getBootcamp()
     }, []);
     
@@ -46,33 +93,106 @@ const Detail = () => {
                 </Link>
                 <Typography color="text.primary">{bootcamp.name}</Typography>
             </Breadcrumbs>
-            <Typography variant="h4" gutterBottom component="div">
-                { bootcamp.name }
-            </Typography>
-            <Typography variant="h6" gutterBottom component="div">
-                Description
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-                {bootcamp.description}
-            </Typography>
-            <Typography variant="h6" gutterBottom component="div">
-                Website
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-                {bootcamp.website}
-            </Typography>
-            <Typography variant="h6" gutterBottom component="div">
-                Phone
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-                {bootcamp.phone}
-            </Typography>
-            <Typography variant="h6" gutterBottom component="div">
-                Email
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-                {bootcamp.email}
-            </Typography>
+            <div style={{display: 'flex'}}>
+                <Typography variant="h4" gutterBottom component="div" style={{flexGrow: 1}}>
+                    { bootcamp.name }
+                </Typography>
+                <Typography variant="button" gutterBottom component="div" style={{flexGrow: 1, textAlign: 'right'}}>
+                    {
+                        editable ? 
+                        <>
+                            <button style={{margin: '5px'}} onClick={saveBootcamp}>SAVE</button>
+                            <button style={{margin: '5px'}} onClick={cancelEditBootcamp}>CANCEL</button>
+                        </> 
+                        :
+                        <>
+                            <button style={{margin: '5px'}} onClick={editBootcamp}>EDIT</button>
+                            <button style={{margin: '5px'}} onClick={removeBootcamp}>REMOVE</button>
+                        </>
+                    }
+                </Typography>
+            </div>
+            {
+                editable ? 
+                <>
+                    <Typography variant="h6" gutterBottom component="div">
+                        Description
+                    </Typography>
+                    <TextField
+                        id="description"
+                        // label="Description"
+                        multiline
+                        fullWidth = "true"
+                        defaultValue={bootcamp.description}
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <Typography variant="h6" gutterBottom component="div">
+                        Website
+                    </Typography>
+                    <TextField
+                        id="website"
+                        // label="Website"
+                        // multiline
+                        fullWidth = "true"
+                        defaultValue={bootcamp.website}
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <Typography variant="h6" gutterBottom component="div">
+                        Phone
+                    </Typography>
+                    <TextField
+                        id="phone"
+                        // label="Website"
+                        // multiline
+                        fullWidth = "true"
+                        defaultValue={bootcamp.phone}
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <Typography variant="h6" gutterBottom component="div">
+                        Email
+                    </Typography>
+                    <TextField
+                        id="email"
+                        // label="Website"
+                        // multiline
+                        fullWidth = "true"
+                        defaultValue={bootcamp.email}
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                </> 
+                :
+                <>
+                    <Typography variant="h6" gutterBottom component="div">
+                        Description
+                    </Typography>
+                    <Typography variant="body1" gutterBottom>
+                        {bootcamp.description}
+                    </Typography>
+                    <Typography variant="h6" gutterBottom component="div">
+                        Website
+                    </Typography>
+                    <Typography variant="body1" gutterBottom>
+                        {bootcamp.website}
+                    </Typography>
+                    <Typography variant="h6" gutterBottom component="div">
+                        Phone
+                    </Typography>
+                    <Typography variant="body1" gutterBottom>
+                        {bootcamp.phone}
+                    </Typography>
+                    <Typography variant="h6" gutterBottom component="div">
+                        Email
+                    </Typography>
+                    <Typography variant="body1" gutterBottom>
+                        {bootcamp.email}
+                    </Typography>
+                </>
+            }
+            
         </Layout>
     )
 }
