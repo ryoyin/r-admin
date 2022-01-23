50698665cfa5a69db55efb6bfa0d1c99653e775d
Roy Ho <kwanyin2000@gmail.com>
2022-01-23 18:30:20 +0000

    bootcamp detail page

--- src/pages/bootcamps/detail.js
+++ src/pages/bootcamps/detail.js
@@ -1,5 +1,7 @@
 import Layout from '../../component/layout'
 import { useHistory, useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import { 
     Link,
@@ -9,7 +11,29 @@ import {
 
 const Detail = () => {
 
+    const [bootcamp, setBootcamp] = useState([])
+
     const { id } = useParams()
+
+    async function getBootcamp() {
+        await axios.get(`http://localhost:9000/api/v1/bootcamps/${id}`)
+        .then(function (response) {
+            // handle success
+            let bootcamp = response.data.data
+            
+            setBootcamp(bootcamp)
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+    }
+
+    useEffect(() => { 
+        // make request for bootcamps
+
+        getBootcamp()
+    }, []);
     
     return (
         <Layout>
@@ -20,10 +44,34 @@ const Detail = () => {
                 <Link underline="hover" color="inherit" href="/bootcamps">
                     Bootcamps
                 </Link>
-                <Typography color="text.primary">{id}</Typography>
+                <Typography color="text.primary">{bootcamp.name}</Typography>
             </Breadcrumbs>
-            <Typography paragraph>
-                Bootcamp - {id}
+            <Typography variant="h4" gutterBottom component="div">
+                { bootcamp.name }
+            </Typography>
+            <Typography variant="h6" gutterBottom component="div">
+                Description
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+                {bootcamp.description}
+            </Typography>
+            <Typography variant="h6" gutterBottom component="div">
+                Website
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+                {bootcamp.website}
+            </Typography>
+            <Typography variant="h6" gutterBottom component="div">
+                Phone
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+                {bootcamp.phone}
+            </Typography>
+            <Typography variant="h6" gutterBottom component="div">
+                Email
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+                {bootcamp.email}
             </Typography>
         </Layout>
     )
